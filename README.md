7e73ded5631073bb069207f839d53f0d296f2401
r1cc4rd0m4zz4 <noreply@example.com>
2023-03-17 23:32:51 +0100

    Aggiunta la possibilità di impostare il constesto della conversazione

--- README.md
+++ README.md
@@ -1,14 +1,16 @@
 # ChAI 
-![visitors](https://visitor-badge.glitch.me/badge?page_id=r1cc4rd0m4zz4/chAI&left_color=green&right_color=red)
+
 ## Introduzione ##
 Un'interfaccia testuale per tenere in una finestra di terminale ChatGPT di OpenAI tramite l'API.
+![Vista da](https://visitor-badge.glitch.me/badge?page_id=r1cc4rd0m4zz4/chAI&left_color=green&right_color=red)
 
 ## Vantaggi ##
- * Utilizzo essenziale della API Key di OpenAI, niente frozoli o interfacce web.
- * Imposta la chiave API OpenAI in una variabile d'ambiente (OPENAI_API_KEY) o come parametro.
- * Comando `clear` per iniziare una nuova chat salvando la precende te in un JSON.
- * Inserire `quit` per salvare la chat in un JSON e tornare al terminale.
- * Ripristinare una precedente chat impostando il parametro con il nome del file JSON di una precedente chat!
+ * Utilizzo essenziale della API Key di OpenAI, niente frozoli o interfacce web
+ * Imposta la chiave API OpenAI in una variabile d'ambiente (OPENAI_API_KEY) o come parametro
+ * Importa i messaggi di conversazione da un file json (.json)
+ * Imposta il contesto della conversazione da un file di testo (.system) o con il comando `system`
+ * Comando `clear` per iniziare una nuova chat salvando la precendente conversazione in un JSON
+ * Inserire `quit` per salvare la chat in un JSON e tornare al terminale
 
 ## Avviso di non responsabilità ##
  * Non sono responsabile della vostra dipendenza da quello che farete con ChatGPT.
@@ -24,17 +26,17 @@ Chattare con ChatGPT API
 options:
   -h, --help         show this help message and exit
   --key KEY          OpenAI API key
-  --importa IMPORTA  Importare un file json con i messaggi
+  --importa IMPORTA  Importa i messaggi di conversazione da un file json (.json)
+  --sistema SISTEMA  Imposta il contesto della conversazione da un file di testo (.system)
   
 ChAI un client python per usare ChatGPT a terminale
 ```
          
 ## Installazione ##
-Per installare ed utilizzare lo script eseguire i seguenti comandi:
+Per installare lo script eseguire i seguenti comandi:
 
 ```bash
 $ git clone https://github.com/r1cc4rd0m4zz4/chAI
 $ cd chAI
 $ pip install -r requirements.txt
-$ python ch.py
 ```
--- ch.py
+++ ch.py
@@ -7,9 +7,10 @@ import json
 from prompt_toolkit import prompt
 from prompt_toolkit.history import FileHistory
 
-parser = argparse.ArgumentParser(description='Chattare con ChatGPT API')
+parser = argparse.ArgumentParser(description='Conversare con ChatGPT API')
 parser.add_argument('--key', type=str, help='OpenAI API key')
-parser.add_argument('--importa', type=str, help='Importare un file json con i messaggi', default=None, required=False)
+parser.add_argument('--importa', type=str, help='Importa i messaggi di conversazione da un file json (.json)', default=None, required=False)
+parser.add_argument('--sistema', type=str, help='imposta il contesto della conversazione da un file di testo (.system)', default=None, required=False)
 
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
@@ -19,13 +20,20 @@ def main(args):
     elif not openai.api_key:
         openai.api_key = args.key
     
+    messages = []
+
     if args.importa and os.path.exists(args.importa) and os.path.isfile(args.importa) and args.importa.endswith('.json') and os.path.getsize(args.importa) > 0:
         file_name = os.path.basename(args.importa)
         with open(file_name, "r", encoding="utf-8") as file:
             json_string = file.read()
             messages = json.loads(json_string, strict=False)
-    else:
-        messages = []
+
+    if args.sistema and os.path.exists(args.sistema) and os.path.isfile(args.sistema) and args.sistema.endswith('.system') and os.path.getsize(args.sistema) > 0:
+        file_name = os.path.basename(args.sistema)
+        with open(file_name, "r", encoding="utf-8") as file:            
+            testo = file.read()
+            messages.append({"role": "system", "content": testo})
+    
     main_prompt(messages)
 
 def main_prompt(messages):
@@ -35,9 +43,14 @@ def main_prompt(messages):
         save_chat(messages)
         sys.exit(1)
     elif text == 'clear':
-        save_chat(messages)
         os.system('cls' if platform.system() == 'Windows' else 'clear')
+        save_chat(messages)
         messages = []
+        main_prompt(messages)
+    elif text == 'system':
+        text = prompt('\nsystem> ', history=history)
+        print(f'')
+        messages.append({"role": "system", "content": text})
         main_prompt(messages)
     else:
         messages.append({"role": "user", "content": text})
@@ -56,10 +69,13 @@ def query_chatgpt(messages):
     messages.append({"role": "assistant", "content": response})
     print(f'\n{response}\n')
     main_prompt(messages)
-
+    
 def save_chat(messages):
-    # Ask for a title
-    messages.append({"role": "user", "content": "Dai un breve titolo alla conversazione"})
+    # se il messages è vuoto non effettuare il salvataggio
+    if len(messages) == 0:
+        return
+    
+    messages.append({"role": "user", "content": "Dai un breve titolo alla conversazione, massimo 24 caratteri"})
     # intercetta l'errore http 4xx/5xx e aggiungi un try/except
     try:
         response = openai.ChatCompletion.create(
@@ -84,6 +100,7 @@ def save_chat(messages):
             file_name = f"{response} ({i}).json"
             if not os.path.exists(file_name):
                 break
+    print(f'\nchat_name> {file_name}\n')
     with open(file_name, "w", encoding="utf-8") as file:
         file.write(json.dumps(messages, ensure_ascii=False, indent=0).replace('\\n', '\n'))
 
